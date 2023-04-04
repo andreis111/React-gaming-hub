9b79f0147b150f3badb686e1fcf63ad8bfde0cfc
Andrei Serban <andrei.sserban@gmail.com>
2023-04-04 13:01:30 +0200

    Filter the games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,7 +33,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.600" />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.600" />
       ))}
     </HStack>
   );
